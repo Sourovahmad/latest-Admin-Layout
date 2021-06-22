381c338344c235ebbe8d94b51437bd1e079d5bed
sourovahmad <mdsourovahmad19@gmail.com>
2021-06-22 20:53:41 +0600

    init

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <script src="{{ asset('admin/jquery/jquery.min.js') }}"></script>
+    <link href="{{ asset('admin/bootstrape5/bootstrap.min.css') }}" rel="stylesheet">
+
+
+    <title>Welcome</title>
+</head>
+<body>
+
+
+@yield('content')
+
+</body>
+</html>
